c07c75cd22e7ca8fed403c5efcc540a98f66d114
Michael Fahim <Michael.Fahim@hva.nl>
2018-05-26 17:55:37 +0200

    Added article and social POST

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const keywords = require('./keywords/keywords.service.js');
 const social = require('./social/social.service.js');
+const article = require('./article/article.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(keywords);
   app.configure(social);
+  app.configure(article);
 };
